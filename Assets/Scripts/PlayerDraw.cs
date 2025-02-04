0b6ce30922fba382c97a49617988473d59b3fcde
MacDudeDude <alexbrhls@gmail.com>
2025-02-04 01:52:28 -0800

    Added drawing
    Also greatly improved performance by splitting the drawing area into multiple tilemaps

--- Assets/Scripts/PlayerDraw.cs
+++ Assets/Scripts/PlayerDraw.cs
@@ -5,9 +5,20 @@ using UnityEngine.Tilemaps;
 
 public class PlayerDraw : MonoBehaviour
 {
+    public float placeRadius;
+    public Tile tile;
     public Grid grid;
-    public Tilemap tilemap;
-    private Vector3Int currentGridPos;
+    public Grid tilemapGrid;
+    public GameObject tilemapPrefab;
+    public int ppu;
+
+    public int rows;
+    public int collumns;
+    public int width;
+    public int height;
+
+    private Vector2 lastMousePosition;
+    private Tilemap[,] tilemaps;
     private int[,] pixelgrid;
     private Camera cam;
 
@@ -15,34 +26,198 @@ public class PlayerDraw : MonoBehaviour
     void Start()
     {
         cam = Camera.main;
+
+        tilemaps = new Tilemap[rows, collumns];
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < collumns; y++)
+            {
+                Tilemap tempMap = Instantiate(tilemapPrefab, Vector3.zero, Quaternion.identity, grid.transform).GetComponent<Tilemap>();
+                tilemaps[x, y] = tempMap;
+            }
+        }
+
+        width *= rows;
+        height *= collumns;
+        pixelgrid = new int[width,height];
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentGridPos = grid.WorldToCell(cam.ScreenToWorldPoint(Input.mousePosition));
-        if(Input.GetMouseButton(0))
-        {
-            PlacePixel(currentGridPos);
-        }else if(Input.GetMouseButton(1))
-        {
-            RemovePixel(currentGridPos);
-        }
+        Adding();
+        Removing();
+
+        //currentGridPos = grid.WorldToCell(cam.ScreenToWorldPoint(Input.mousePosition));
+
+        //if(Input.GetMouseButton(0))
+        //{
+        //    PlacePixel(currentGridPos, currentTileMapPos);
+        //}else if(Input.GetMouseButton(1))
+        //{
+        //    RemovePixel(currentGridPos, currentTileMapPos);
+        //}
     }
 
-    public void PlacePixel(Vector3Int pos)
+    public void PlacePixel(Vector3Int pos, Vector3Int tileMapPos)
     {
+        if (pos.x < 0 || pos.x >= width || pos.y < 0 || pos.y >= height)
+            return;
         if (pixelgrid[pos.x, pos.y] == 1)
             return;
 
-
+        pixelgrid[pos.x, pos.y] = 1;
+        tilemaps[tileMapPos.x, tileMapPos.y].SetTile(pos, tile);
     }
 
-    public void RemovePixel(Vector3Int pos)
+    public void RemovePixel(Vector3Int pos, Vector3Int tileMapPos)
     {
+        if (pos.x < 0 || pos.x >= width || pos.y < 0 || pos.y >= height)
+            return;
         if (pixelgrid[pos.x, pos.y] == 0)
             return;
 
+        pixelgrid[pos.x, pos.y] = 0;
+        tilemaps[tileMapPos.x, tileMapPos.y].SetTile(pos, null);
+    }
 
+    private void Adding()
+    {
+        if (Input.GetMouseButtonDown(0))
+            lastMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButton(0))
+        {
+            float currentPlaceRadius = placeRadius * (1f / ppu);
+
+            int gridRadius = Mathf.CeilToInt(currentPlaceRadius / (1f / ppu));
+            float squaredRadius = Mathf.Pow(currentPlaceRadius, 2);
+
+            Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridStartpoint = grid.WorldToCell(lastMousePosition);
+            Vector3Int gridEndpoint = grid.WorldToCell(mousePos);
+            mousePos = grid.CellToWorld(gridEndpoint);
+
+            List<Vector2Int> line = GenerateLine(gridStartpoint.x, gridStartpoint.y, gridEndpoint.x, gridEndpoint.y);
+            for (int i = 0; i < line.Count; i++)
+            {
+                Vector3Int centerGridPos = (Vector3Int)line[i];
+                Vector2 centerGridWorldPos = grid.CellToWorld(centerGridPos);
+                for (int x = -gridRadius; x <= gridRadius; x++)
+                {
+                    for (int y = -gridRadius; y <= gridRadius; y++)
+                    {
+                        Vector3Int currentGridPos = new Vector3Int(centerGridPos.x + x, centerGridPos.y + y);
+                        Vector2 worldPos = grid.CellToWorld(currentGridPos);
+
+                        if ((worldPos - centerGridWorldPos).sqrMagnitude <= squaredRadius)
+                        {
+                            if (currentGridPos.x < 0 || currentGridPos.y < 0 || currentGridPos.x >= width || currentGridPos.y >= height)
+                                continue;
+
+                            Vector3Int currentTileMapPos = tilemapGrid.WorldToCell(worldPos);
+
+                            if (pixelgrid[currentGridPos.x, currentGridPos.y] == 1)
+                                continue;
+
+                            pixelgrid[currentGridPos.x, currentGridPos.y] = 1;
+                            tilemaps[currentTileMapPos.x, currentTileMapPos.y].SetTile(currentGridPos, tile);
+                        }
+                    }
+                }
+            }
+
+            lastMousePosition = mousePos;
+        }
+    }
+
+    private void Removing()
+    {
+        if (Input.GetMouseButtonDown(1))
+            lastMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButton(1))
+        {
+            float currentPlaceRadius = placeRadius * (1f / ppu);
+
+            int gridRadius = Mathf.CeilToInt(currentPlaceRadius / (1f / ppu));
+            float squaredRadius = Mathf.Pow(currentPlaceRadius, 2);
+
+            Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridStartpoint = grid.WorldToCell(lastMousePosition);
+            Vector3Int gridEndpoint = grid.WorldToCell(mousePos);
+            mousePos = grid.CellToWorld(gridEndpoint);
+
+            List<Vector2Int> line = GenerateLine(gridStartpoint.x, gridStartpoint.y, gridEndpoint.x, gridEndpoint.y);
+            for (int i = 0; i < line.Count; i++)
+            {
+                Vector3Int centerGridPos = (Vector3Int)line[i];
+                Vector2 centerGridWorldPos = grid.CellToWorld(centerGridPos);
+                for (int x = -gridRadius; x <= gridRadius; x++)
+                {
+                    for (int y = -gridRadius; y <= gridRadius; y++)
+                    {
+                        Vector3Int currentGridPos = new Vector3Int(centerGridPos.x + x, centerGridPos.y + y);
+                        Vector2 worldPos = grid.CellToWorld(currentGridPos);
+
+                        if ((worldPos - centerGridWorldPos).sqrMagnitude <= squaredRadius)
+                        {
+                            if (currentGridPos.x < 0 || currentGridPos.y < 0 || currentGridPos.x >= width || currentGridPos.y >= height)
+                                continue;
+
+                            Vector3Int currentTileMapPos = tilemapGrid.WorldToCell(worldPos);
+
+                            if (pixelgrid[currentGridPos.x, currentGridPos.y] == 0)
+                                continue;
+
+                            pixelgrid[currentGridPos.x, currentGridPos.y] = 0;
+                            tilemaps[currentTileMapPos.x, currentTileMapPos.y].SetTile(currentGridPos, null);
+                        }
+                    }
+                }
+            }
+
+            lastMousePosition = mousePos;
+        }
+    }
+
+    public List<Vector2Int> GenerateLine(int x, int y, int x2, int y2)
+    {
+        List<Vector2Int> linePositions = new List<Vector2Int>();
+
+        int w = x2 - x;
+        int h = y2 - y;
+        int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+        if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
+        if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
+        if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
+        int longest = Mathf.Abs(w);
+        int shortest = Mathf.Abs(h);
+        if (!(longest > shortest))
+        {
+            longest = Mathf.Abs(h);
+            shortest = Mathf.Abs(w);
+            if (h < 0) dy2 = -1; else if (h > 0) dy2 = 1;
+            dx2 = 0;
+        }
+        int numerator = longest >> 1;
+        for (int i = 0; i <= longest; i++)
+        {
+            linePositions.Add(new Vector2Int(x, y));
+            numerator += shortest;
+            if (!(numerator < longest))
+            {
+                numerator -= longest;
+                x += dx1;
+                y += dy1;
+            }
+            else
+            {
+                x += dx2;
+                y += dy2;
+            }
+        }
+
+        return linePositions;
     }
 }
