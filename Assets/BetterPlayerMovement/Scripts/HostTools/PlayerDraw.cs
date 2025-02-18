e6b7bff92b988d3a721b7392b780d1bbec8c6b39
MacDudeDude <alexbrhls@gmail.com>
2025-02-18 01:57:06 -0800

    Small test

--- Assets/BetterPlayerMovement/Scripts/HostTools/PlayerDraw.cs
+++ Assets/BetterPlayerMovement/Scripts/HostTools/PlayerDraw.cs
@@ -528,22 +528,24 @@ public class PlayerDraw : NetworkBehaviour
     /// Target RPC to send stored drawing commands to a specific client
     /// </summary>
     [TargetRpc]
-    private void TargetSendStoredCommands(NetworkConnection target, Color32[] colors)
+    private void TargetSendStoredCommands(NetworkConnection target, string gridString)
     {
-        var unflattendedColors = ArrayFlattener.Unflatten(colors, width, height);
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                Vector3 worldPos = grid.CellToWorld(new Vector3Int(x, y));
-                Vector3Int currentTileMapPos = tilemapGrid.WorldToCell(worldPos);
+        Debug.Log(gridString);
 
-                if(unflattendedColors[x, y].a != 0)
-                {
-                    QueTile(currentTileMapPos.x, currentTileMapPos.y, new Vector3Int(x, y), tileValues[1], 1, collisionLayer, unflattendedColors[x, y]);
-                }
-            }
-        }
+        //var unflattendedColors = ArrayFlattener.Unflatten(colors, width, height);
+        //for (int x = 0; x < width; x++)
+        //{
+        //    for (int y = 0; y < height; y++)
+        //    {
+        //        Vector3 worldPos = grid.CellToWorld(new Vector3Int(x, y));
+        //        Vector3Int currentTileMapPos = tilemapGrid.WorldToCell(worldPos);
+
+        //        if(unflattendedColors[x, y].a != 0)
+        //        {
+        //            QueTile(currentTileMapPos.x, currentTileMapPos.y, new Vector3Int(x, y), tileValues[1], 1, collisionLayer, unflattendedColors[x, y]);
+        //        }
+        //    }
+        //}
     }
 
     /// <summary>
@@ -552,6 +554,6 @@ public class PlayerDraw : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     public void RequestStoredCommandsServerRpc(NetworkConnection sender = null)
     {
-        TargetSendStoredCommands(sender, textureColors[collisionLayer]);
+        TargetSendStoredCommands(sender, "asdawdasdawdasdawdasdwaadsalkdwqhdqkjbdkasjbdkajwbdqkjbdskqjbqdkjqbwdkjqbdkjb");
     }
 }
