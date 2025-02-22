d480faa4866e7bce20914e08347d9c269c42231a
MacDudeDude <alexbrhls@gmail.com>
2025-02-22 13:38:02 -0800

    Added color palette ui

---
+++ Assets/Scripts/UI/ColorHolder.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorHolder : MonoBehaviour
+{
+    public GameUIManager uiManager;
+    public Color32 color;
+    public Image image;
+
+    public void SetColor(Color32 color)
+    {
+        this.color = color;
+        image.color = color;
+    }
+
+    public void SendColor(bool isOn)
+    {
+        if(isOn)
+            uiManager.SetPenColor(color);
+    }
+
+    public void OnValidate()
+    {
+        if(image != null)
+        {
+            image.color = color;
+        }
+    }
+}
